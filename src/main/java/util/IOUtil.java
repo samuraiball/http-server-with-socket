a364ebdd41d7e89f43f531c474105c71518d00da
Taichi Uragami <backpaper0@gmail.com>
2019-05-21 06:38:35 +0900

    Improve readLine method

--- src/main/java/util/IOUtil.java
+++ src/main/java/util/IOUtil.java
@@ -5,8 +5,6 @@ import exception.EmptyRequestException;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class IOUtil {
@@ -28,34 +26,22 @@ public class IOUtil {
     }
 
     public static String readLine(InputStream in) throws IOException {
-        List<Byte> list = new ArrayList<>();
-
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
         while (true) {
-            byte b = (byte) in.read();
-
+            int b = in.read();
             if (b == -1) {
                 throw new EmptyRequestException();
-            }
-
-            list.add(b);
-
-            int size = list.size();
-            if (2 <= size) {
-                char cr = (char) list.get(size - 2).byteValue();
-                char lf = (char) list.get(size - 1).byteValue();
-
-                if (cr == '\r' && lf == '\n') {
-                    break;
+            } else if (b == '\r') {
+                b = in.read();
+                if (b == '\n') {
+                    return out.toString("UTF-8");
+                } else if (b == -1) {
+                    throw new EmptyRequestException();
                 }
+                out.write('\r');
             }
+            out.write(b);
         }
-
-        byte[] buffer = new byte[list.size() - 2]; // CRLF の分減らす
-        for (int i = 0; i < list.size() - 2; i++) {
-            buffer[i] = list.get(i);
-        }
-
-        return new String(buffer, UTF_8);
     }
 
     public static InputStream toInputStream(String string) {
